5f6491851929710b2272d321c94cddf1fc3d3a5e
P Luong <p-luong@live.co.uk>
2022-10-04 18:05:41 +0100

    added session object to server.js

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
-// Dependancies 
-const path = require('path');
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// Dependancies
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Set up the Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set handlebars.js as the default template emgine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
-// 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
-
 // Sync sequelize and start the server to begin listening
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
