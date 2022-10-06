183bce4f8bc6555ee31dbce0b71a478ce00f8658
P Luong <p-luong@live.co.uk>
2022-10-06 15:02:44 +0100

    added signup route to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('home', { 
       posts, 
       logged_in: req.session.logged_in 
     });
@@ -78,5 +78,15 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('signup');
+  });
 
 module.exports = router;
