3fbccc8c53be7e5ee6f15909cd7bac786d1bc691
P Luong <p-luong@live.co.uk>
2022-10-06 20:44:57 +0100

    updated route to edit posts

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
@@ -23,6 +23,24 @@ router.get('/create', withAuth, (req, res) => {
     res.render('create', {
         logged_in: req.session.logged_in 
       });
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id);
+      
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+      const post = postData.get({ plain: true });
+      res.render('edit', {
+        ...post,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.post('/login', async (req, res) => {
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,6 +21,31 @@ const newPostHandler = async (event) => {
         }
     }
   };
+
+  const editPostHandler = async (event) => {
+    console.log("Edit post handler received!");
+    console.log(event);
+  
+    const title = document.querySelector('#new-post-title').value.trim();
+    const content = document.querySelector('#new-post-content').value.trim();
+  
+    if (title && content) {
+        console.log(title + content);
+         const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: {
+            'Content-Type': 'application/json',
+            },
+      });
+    
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create post');
+        }
+    }
+  };
   
   const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
@@ -42,7 +67,7 @@ const newPostHandler = async (event) => {
     .querySelector('.create-new-post')
     .addEventListener('submit', newPostHandler);
   
-  document
-    .querySelector('.project-list')
-    .addEventListener('click', delButtonHandler);
+    document
+    .querySelector('.edit-user-post')
+    .addEventListener('click', editPostHandler);
   
