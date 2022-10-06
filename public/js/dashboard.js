5f393e0ab4c1afafaf946ace47efb2b98ff4d679
P Luong <p-luong@live.co.uk>
2022-10-06 19:12:49 +0100

    linked create new post front end to back end route

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,24 +1,24 @@
-const newFormHandler = async (event) => {
+const newPostHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const title = document.querySelector('#new-post-title').value.trim();
+    const content = document.querySelector('#new-post-content').value.trim();
   
-    if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
-        method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    if (title && content) {
+        console.log(title + content);
+         const response = await fetch(`/api/posts`, {
+            method: 'POST',
+            body: JSON.stringify({ title, content }),
+            headers: {
+            'Content-Type': 'application/json',
+            },
       });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create project');
-      }
+    
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to create post');
+        }
     }
   };
   
@@ -39,8 +39,8 @@ const newFormHandler = async (event) => {
   };
   
   document
-    .querySelector('.new-project-form')
-    .addEventListener('submit', newFormHandler);
+    .querySelector('.create-new-post')
+    .addEventListener('submit', newPostHandler);
   
   document
     .querySelector('.project-list')
