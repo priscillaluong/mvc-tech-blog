652403ce38a0a7f1f4cd0d399a1686c3c061133a
P Luong <p-luong@live.co.uk>
2022-10-06 14:14:53 +0100

    amended signup handlebars and js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,9 +1,9 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+
+    const name = document.querySelector('#inputName-signup').value.trim();
+    const email = document.querySelector('#inputEmail-signup').value.trim();
+    const password = document.querySelector('#inputPassword-signup').value.trim();
   
     if (name && email && password) {
       const response = await fetch('/api/users', {
@@ -13,7 +13,7 @@ const signupFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
