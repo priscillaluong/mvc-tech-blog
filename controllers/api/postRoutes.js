0bed000b37e7c8403b4e738062334ab81cd24eda
P Luong <p-luong@live.co.uk>
2022-10-07 16:18:55 +0100

    added comment routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -87,6 +87,39 @@ router.put('/:id', withAuth, async (req, res) => {
 
 /* COMMENTS ROUTES */
 
+router.post('/:id', withAuth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        user_id: req.session.user_id,
+        post_id: req.params.id,
+      });
+      console.log("--------------------------------");
+      console.log(newComment);
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
+  router.get('/:id', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const commentData = await Comment.findAll({
+        where: { post_id: req.params.id } 
+      });
+      console.log(commentData);
+      // Serialize data so the template can read it
+      const comments = commentData.map((comment) => comment.get({ plain: true }));
+      console.log(comments);
+      // Pass serialized data and session flag into template
+      res.render('comment', { 
+        comments, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
---
+++ public/js/comment.js
@@ -0,0 +1,35 @@
+async function newCommentHandler(event) {
+    event.preventDefault();
+  
+    const content = document.querySelector('#new-comment-content').value.trim();
+
+    if (content) {
+        console.log(content);
+        if (event.target.hasAttribute('data-id')) {
+            const id = event.target.getAttribute('data-id');
+            const response = await fetch(`/api/posts/${id}`, {
+                method: 'POST',
+                body: JSON.stringify({ content }),
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            });
+            console.log(response);
+        if (response.ok) {
+            console.log("All ok!")
+            /* document.location.reload(); */
+        } else {
+            alert('Failed to add comment');
+        }
+    }
+  };
+};
+
+/* function newCommentHandler(e){
+    e.preventDefault();
+    console.log("New comment button clicked");
+} */
+
+  document
+    .querySelector('.new-comment-btn')
+    .addEventListener('click', newCommentHandler);
